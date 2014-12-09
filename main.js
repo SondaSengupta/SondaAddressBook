5db224f5ef730f1103e7fa049af970ce5e6deeb4
SondaSengupta <sondasengupta@gmail.com>
2014-12-09 11:26:40 -0600

    Added Birthday Form using AngularStrap

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 ;(function(){
   'use strict';
-  angular.module('AddressBook', ['ngRoute'])
+  angular.module('AddressBook', ['ngRoute','mgcrea.ngStrap'])
  .config(function($routeProvider){
       $routeProvider
       .when('/', {
